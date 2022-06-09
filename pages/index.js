7e0e2a1c8d5946b7f2ee8d1cef9876d1fb736843
Andreas <74594568+Relouding@users.noreply.github.com>
2022-06-09 12:29:50 +0200

    Second push

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,37 @@
-import Head from "next/head";
+import Head from "../components/layout/Head";
 import Layout from "../components/layout/Layout";
+import axios from "axios";
+import { BASE_URL } from "../constants/api";
 
-export default function Home() {
- return (
-  <Layout>
-   <Head />
+export default function Index(props) {
+  // the log here will happen in the browser console
+  console.log(props);
 
-   <div className="container">
-    <h1>Home page</h1>
-   </div>
-  </Layout>
- );
+  return (
+    <Layout>
+      <Head title="Next Intro" />
+
+      {props.posts.map((post) => {
+        return <h3 key={post.slug}>{post.title}</h3>;
+      })}
+    </Layout>
+  );
+}
+
+export async function getServerSideProps() {
+  let posts = [];
+
+  try {
+    const response = await axios.get(BASE_URL);
+    console.log(response.data);
+    posts = response.data.results;
+  } catch (error) {
+    console.log(error);
+  }
+
+  return {
+    props: {
+        posts: posts,
+    },
+  };
 }
